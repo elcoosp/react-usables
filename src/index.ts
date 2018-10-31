aa4cd71969ec41c3eec45a310d24baf88b8e8ade
Louis Cospain <elcoosp@gmail.com>
2018-10-31 23:31:08 +0100

    feat: 🎸 Add TS definitions for useMergedState and useTogglable

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 
-export function useMergedState(initialState) {
+// Plain old JS object
+interface Pojo {
+	[key: string]: any
+}
+
+export function useMergedState(
+	initialState: Pojo
+): [Pojo, (newState: Pojo) => void] {
 	const [state, update] = useState(initialState)
 	return [state, newState => update({ ...state, ...newState })]
 }
 
-export function useTogglable(initialState) {
+export function useTogglable(initialState: boolean): [boolean, () => void] {
 	const [isToggled, toggle] = useState(initialState)
 	return [isToggled, () => toggle(!isToggled)]
 }
